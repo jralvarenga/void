90fc94cc9b8ee634ab2c3fc1c4e7cd681a3c38fb
Rigo Alvarenga <jralvarenga161@gmail.com>
2024-04-25 14:25:48 -0600

    feat(searchbar): adding searchbar

--- web/src/app/(void)/void/page.tsx
+++ web/src/app/(void)/void/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { FormEvent, useCallback, useEffect, useRef, useState } from 'react'
-import ogs from 'open-graph-scraper'
+import { FormEvent, useRef, useState } from 'react'
+import { Search, Plus } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export default function VoidPage() {
   const [searchValue, setSearchValue] = useState('')
@@ -40,22 +42,25 @@ export default function VoidPage() {
   return (
     // search bar
     <div className="flex w-full items-center justify-center">
-      <div className=" flex w-full max-w-xl flex-col">
-        {/* <div className="searchbar-shadow  bg-background relative flex h-14 w-full rounded-full"> */}
-        <form
-          className="bg-background relative flex h-14 w-full gap-3 rounded-full"
-          onSubmit={getUrlMetadata}
-        >
-          <input
+      <div className=" flex w-full max-w-xl items-center gap-2">
+        <form className="searchbar-shadow relative flex h-12 w-full items-center gap-3 rounded-full bg-background">
+          <Search className="absolute left-4 h-4 w-4 text-muted-foreground" />
+          <Input
             type="text"
             ref={searchInput}
-            className="h-full w-full rounded-full border bg-inherit p-4 duration-300 focus:-mt-3 focus:drop-shadow-xl"
+            className="h-full w-full rounded-full border bg-inherit p-3 pl-10 duration-300 focus:-mt-3 focus:pl-3 focus:drop-shadow-xl"
             placeholder="search in your void..."
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
           />
-          <button>search</button>
         </form>
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-12 w-14 rounded-full"
+        >
+          <Plus className="h-6 w-6" />
+        </Button>
       </div>
     </div>
   )
